b4c4051cfbe02eed7fa59c9d42181452b4d9b0c5
Gabor Javorszky <gabor@javorszky.co.uk>
2021-10-06 15:33:37 +0100

    Add more signup relation bits

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,49 +1,98 @@
-import { useState } from 'react'
-import { useAuth } from './use-auth'
+import { useState } from "react";
+import { useAuth } from "./use-auth";
+import { supabase } from "./supabase.js";
+import { useLocation } from "react-router-dom";
 
-function Signup() {
-    const auth = useAuth()
-    const [email, setEmail] = useState('')
-    const [pw, setPw] = useState('')
-
-    return (
-        <>
-            <h1 className="title">Signup</h1>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    auth.signup(email, pw)
-                }}>
-                <div className="field">
-                    <label className="label" htmlFor="email">Email address</label>
-                    <div className="control">
-                        <input
-                            className="input"
-                            type="email"
-                            name="email"
-                            id="email"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                </div>
-                <div className="field">
-                    <label className="label" htmlFor="password">Password</label>
-                    <div className="control">
-                        <input
-                            className="input"
-                            type="password"
-                            name="password"
-                            id="password"
-                            onChange={(e) => setPw(e.target.value)}
-                        />
-                    </div>
-                </div>
-                <div className="field">
-                    <button className="button is-primary" type="submit">Sign up</button>
-                </div>
-            </form>
-        </>
-    )
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
 }
 
-export default Signup
+function Signup() {
+  const auth = useAuth();
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  const query = useQuery();
+  const inviter = query.get("inviter");
+
+  const checkInvite = async (uid) => {
+    const { data, error, count } = await supabase
+      .from("profiles")
+      .select(
+        `
+      id,
+      employee_id:id!employee_id (employee_id)
+      `
+      )
+      .eq("id", uid)
+      .is("employee_id", null);
+
+    console.log("inviter check", data, error, count);
+    // if (error || !count) {
+    //   auth.setNotification({
+    //     type: "is-danger",
+    //     message: "You have an invite referral, but that is invalid.",
+    //   });
+    //   return false;
+    // }
+
+    return uid;
+  };
+
+  if (inviter !== null) {
+    console.log("checking inviter");
+    checkInvite(inviter);
+  }
+
+  console.log(query.get("inviter"));
+
+  return (
+    <>
+      <h1 className="title">Signup</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          auth.signup(email, pw);
+        }}
+      >
+        <div className="field">
+          <label className="label" htmlFor="email">
+            Email address
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label" htmlFor="password">
+            Password
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => setPw(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-primary" type="submit">
+            Sign up
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Signup;
